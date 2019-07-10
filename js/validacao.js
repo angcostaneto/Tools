60303a76f6b20a94f5991b924d3b4c57f164f24c
Ângelo Costa Neto <angelo_costa_neto@outlook.com>
2019-07-10 10:18:34 -0300

    Adiciona validação em js

---
+++ js/validacao.js
@@ -0,0 +1,6 @@
+/**
+ * Verifica se o email é do gmail, se ele termina exatamente com @gmail.com ou @GMAIL.COM  
+ */
+if (/^([A-Za-z0-9_\-\.])+\@([gmail|GMAIL])+\.(com)$/.test(input.value)) {
+  // Codigo
+}
